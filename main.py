1b0e6b8c928820bcb770e64256e76623fffb18fc
morozgit <morozec76@gmail.com>
2023-10-03 18:24:59 +0300

    add tg start

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
+import os
+
 import requests
+from telegram.ext import Updater
+from telegram import Update
+from telegram.ext import CallbackContext
+from telegram.ext import CommandHandler
+from dotenv import find_dotenv, load_dotenv
+
+
+def start(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f'Hello {update.message.from_user.name}')
 
 
 def main():
+    load_dotenv(find_dotenv())
+    token_devman = os.environ.get("TOKEN_DEVMAN")
+    TG_token = os.environ.get("TG_TOKEN")
+    updater = Updater(token=TG_token, use_context=True)
+    dispatcher = updater.dispatcher
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+    updater.start_polling()
     headers = {
-        'Authorization': 'Token b59cc5445ea444f525c516c39cab691038092c94'
+        'Authorization': token_devman
     }
     url = 'https://dvmn.org/api/long_polling/'
+    payload = {
+        'timestamp': '1555493856'
+    }
     while (True):
-        response = requests.get(url=url, headers=headers)
-        response.raise_for_status()
-        print(response.json())
-
+        try:
+            response = requests.get(url=url,
+                                    headers=headers,
+                                    params=payload,
+                                    timeout=5,
+                                    )
+            response.raise_for_status()
+            print(response.json())
+        except requests.exceptions.ReadTimeout as err:
+            print(err)
+        except requests.exceptions.ConnectionError as err:
+            print(err)
 
 if __name__ == '__main__':
     main()
